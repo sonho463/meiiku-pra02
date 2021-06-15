ddb313eaa0402423a16385289c3df346b9646096
sonho463 <son.ho463@gmail.com>
2021-06-15 22:36:05 +0900

    カスタムタクソノミーのカテゴリとタグの表示

--- archive-materials.php
+++ archive-materials.php
@@ -43,9 +43,48 @@ if (have_posts()) : ?>
 					<li>
 						<?php the_author(); ?>
 					</li>
-					<li><?php the_taxonomies(); ?> </li>
-				</ul>
+					<li>
+						<?php
+						$taxonomy = 'material_genre';
 
+						// 投稿に付けられたターム（カテゴリー）の ID を取得する。
+						$post_terms = wp_get_object_terms($post->ID, $taxonomy, array('fields' => 'ids'));
+						// リンクの区切り文字
+						$separator = ', ';
+
+						if (!empty($post_terms) && !is_wp_error($post_terms)) {
+
+							$term_ids = implode(',', $post_terms);
+							$terms = wp_list_categories('title_li=&style=none&echo=0&taxonomy=' . $taxonomy . '&include=' . $term_ids);
+							$terms = rtrim(trim(str_replace('<br />',  $separator, $terms)), $separator);
+
+							// 投稿のカテゴリーを表示
+							echo  $terms;
+						}
+						?>
+					</li>
+					<li>
+						<?php
+						$taxonomy = 'material_tag';
+
+						// 投稿に付けられたターム（カテゴリー）の ID を取得する。
+						$post_terms = wp_get_object_terms($post->ID, $taxonomy, array('fields' => 'ids'));
+						// リンクの区切り文字
+						$separator = ', ';
+
+						if (!empty($post_terms) && !is_wp_error($post_terms)) {
+
+							$term_ids = implode(',', $post_terms);
+							$terms = wp_list_categories('title_li=&style=none&echo=0&taxonomy=' . $taxonomy . '&include=' . $term_ids);
+							$terms = rtrim(trim(str_replace('<br />',  $separator, $terms)), $separator);
+							$terms = str_replace('">','">#',$terms);
+
+							// 投稿のカテゴリーを表示
+							echo  $terms;
+						}
+						?>
+					</li>
+				</ul>
 				<hr>
 			</div>
 
---
+++ taxonomy.php
@@ -0,0 +1,102 @@
+<?php get_header(); ?>
+<header>
+	<?php get_template_part('includes/header'); ?>
+</header>
+
+<?php
+if (have_posts()) :
+?>
+
+	<div class="content-width">
+		<h1>
+			<?php
+			if (is_taxonomy('material_genre')) {
+				echo 'カテゴリ「' . single_term_title('', false) .  '」の投稿一覧';
+			} elseif (is_tag()) {
+				echo 'タグ「' . single_term_title('', false) .  '」の投稿一覧';
+			} elseif (is_day()) {
+				echo '「' . get_the_date('Y年n月j日') .  '」の投稿一覧';
+			} elseif (is_month()) {
+				echo '「' . get_the_date('Y年n月') .  '」の投稿一覧';
+			} elseif (is_year()) {
+				echo '「' . get_the_date('Y年') .  '」の投稿一覧';
+			} else {
+				echo 'blog';
+			}
+			?>
+		</h1>
+		<h2>archive.phpです</h2>
+	</div><!-- /.content-width -->
+
+
+	<?php
+	while (have_posts()) :
+		the_post();
+	?>
+
+
+
+		<div class="content-width">
+			<a href="<?php echo the_permalink() ?>">
+				<h3><?php the_title(); ?></h3>
+			</a>
+			<ul>
+				<li>
+					<p><?php the_author(); ?></p>
+				</li>
+				<li>
+					カテゴリ：<?php the_category('／', '<span>'); ?>
+				</li>
+				<li>
+					タグ：
+					<?php
+					$tags = get_the_tags();
+					if ($tags) :
+						foreach ($tags as $tag) :
+							$name = $tag->name;
+							$slug = $tag->slug;
+					?>
+							<a href="<?php get_template_directory_uri(); ?>/tag/<?php echo $slug; ?>">#<?php echo $name; ?></a>,
+						<? endforeach; ?>
+					<? endif; ?>
+				</li>
+				<li>
+					<?php
+					$year = get_the_date('Y');
+					$month = get_the_date('m');
+					$day = get_the_date('d');
+					?>
+					<a href="<?php echo get_year_link($year); ?>;">
+						<span><?php echo get_the_date('Y'); ?></span>
+					</a>年
+					<a href="<?php echo get_month_link($year, $month); ?>;">
+						<span><?php echo get_the_date('m'); ?></span>
+					</a>月
+					<a href="<?php echo get_day_link($year, $month, $day); ?>;">
+						<span><?php echo get_the_date('d'); ?></span>
+					</a>日
+				</li>
+
+			</ul>
+		</div>
+
+
+		<hr>
+		<?php
+		wp_reset_postdata();
+		?>
+
+
+
+<?php
+	endwhile;
+endif;
+?>
+
+
+
+<?php get_footer(); ?>
+
+</body>
+
+</html>
